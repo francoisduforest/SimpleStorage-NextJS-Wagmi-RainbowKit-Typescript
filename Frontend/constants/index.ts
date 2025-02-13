37c348039ef8c228fbbe1a44ea50535607b51e49
François Duforest <francois.duforest@decathlon.com>
2025-02-13 09:02:58 +0100

    update to react 19

--- Frontend/constants/index.ts
+++ Frontend/constants/index.ts
@@ -1,1 +1,31 @@
 // Auto-generated constants for Storage contract on localhost network
+export const contractAbi = [
+  {
+    "inputs": [],
+    "name": "retrieve",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "num",
+        "type": "uint256"
+      }
+    ],
+    "name": "store",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+] as const;
+
+export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3" as const;
